069d91c0e9951b0463210809528fdf01144de666
Mikita Piastou <mpiasto1@my.westga.edu>
2024-11-21 22:13:02 -0700

    Added social media icons

--- client/src/components/main/Footer.tsx
+++ client/src/components/main/Footer.tsx
@@ -1,24 +1,70 @@
 import { Container, Nav, Navbar } from "react-bootstrap";
-import logo from "../../assets/main/logo.png";
+import {
+  FaFacebookF,
+  FaTwitter,
+  FaInstagram,
+  FaLinkedinIn,
+  FaYoutube,
+} from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <Navbar variant="dark" className="tw-mt-8">
+    <Navbar variant="dark" className="tw-mt-8 tw-py-4" expand="lg">
       <Container className="tw-flex tw-justify-between tw-items-center">
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <img src={logo} alt="logo" className="tw-w-8" />
+        <div className="tw-flex tw-space-x-4">
+          <a
+            href="https://linkedin.com"
+            className="text-light"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaLinkedinIn size={20} />
+          </a>
+          <a
+            href="https://facebook.com"
+            className="text-light"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaFacebookF size={20} />
+          </a>
+          <a
+            href="https://instagram.com"
+            className="text-light"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaInstagram size={20} />
+          </a>
+          <a
+            href="https://twitter.com"
+            className="text-light"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaTwitter size={20} />
+          </a>
+          <a
+            href="https://youtube.com"
+            className="text-light"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaYoutube size={20} />
+          </a>
+        </div>
         <Navbar.Collapse
           id="basic-navbar-nav"
           className="tw-flex tw-justify-center"
         >
           <Nav>
-            <Nav.Link className="footer-link" href="news">
+            <Nav.Link className="footer-link" href="/news">
               News
             </Nav.Link>
-            <Nav.Link className="footer-link" href="conditions">
+            <Nav.Link className="footer-link" href="/conditions">
               Terms & Conditions
             </Nav.Link>
-            <Nav.Link className="footer-link" href="privacy">
+            <Nav.Link className="footer-link" href="/privacy">
               Privacy Notice
             </Nav.Link>
           </Nav>
